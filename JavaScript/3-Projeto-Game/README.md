00f770fd6db87224de1dd56f4f75542e9c940ce6
MariliaMSiqueira <mariliamatiassiqueira98@gmail.com>
2022-03-17 18:57:42 -0300

    adding new project JS

---
+++ JavaScript/3-Projeto-Game/README.md
@@ -0,0 +1,5 @@
+<div align="center"><h1>Game -  Mata Mosquito</h1></div>
+
+---
+
+<div align="center"><h2>em construção .... 🛠⚙</h2></div>
---
+++ JavaScript/3-Projeto-Game/assets/js/script.js
@@ -0,0 +1,130 @@
+//ESCOPO GLOBAL
+//DEFININDO AS DIMENSÕES DO BROWSER
+let width = 0;
+let height = 0;
+let life = 1;
+let time = 10;
+//ESCOPO DA FUNÇÃO
+//INVOCADA NO BODY ONRESIZE
+function getDimensions(){
+  width = window.innerWidth;
+  height = window.innerHeight;
+  
+};
+
+getDimensions();
+
+
+function startGame(){
+  alert(document.getElementById('level').value)
+}
+
+let createFly = setInterval(() => {
+  randomPosition()
+}, 2000);
+//CRONÔMETRO VITÓRIA
+let stopwatch = setInterval(() => { 
+  time -= 1
+  if(time < 0){
+    clearInterval(stopwatch);
+    clearInterval(createFly);
+    window.location.href="winner.html";
+  }else{
+    document.getElementById('stopwatch').innerHTML = time;
+  }
+}, 1000)
+
+
+
+//DEFININDO POSIÇÕES RANDÔMICAS
+//ARREDONDANDO PARA BAIXO
+//-90 = MARGEM PARA O TAMANHO DA IMAGEM MOSQUITO
+function randomPosition() {
+
+  //REMOVER O MOSQUITO ANTERIOR (CASO EXISTA)
+  //SE O ELEMENTO ANTERIOR NÃO EXISTE, NÃO ENTRARÁ NESTA LÓGICA
+  //SE O USUÁRIO CLICAR, O ELEMENTO NÃO EXISTIRÁ.
+  if(document.getElementById('fly')){
+    document.getElementById('fly').remove();
+    //ALTERANDO ORIGEM
+    //ESTRUTURA PARA TORNAR O ID DINÂMICO
+    //DESTA FORMA, FOI CRIADO UM CONTADOR QUE FOI CONCATENADO AO ID
+    if(life > 3){
+      window.location.href="gameover.html"
+    }else{
+    document.getElementById('life' + life).src="assets/img/coracao_vazio.png"
+    life++
+    };
+    
+  };
+
+  let positionX = Math.floor(Math.random() * width) - 90;
+  let positionY = Math.floor(Math.random() * height) - 90;
+  // OPERADOR TERNÁRIO
+  //LEITURA = SE POSITION FOR MENOR QUE ZERO, RECEBE 0, SENÃO RECEBE ELA MESMO
+  positionX = positionX < 0 ? 0 : positionX;
+  positionY = positionY < 0 ? 0 : positionY;
+
+
+  //CRIANDO ELEMENTOS HTML
+
+  let flyImg = document.createElement('img');
+  flyImg.src = 'assets/img/mosca.png';
+  flyImg.className = randomFlySize() + ' ' +randomSide();
+  flyImg.style.left = positionX + 'px';
+  flyImg.style.top = positionY + 'px';
+  flyImg.style.position = 'absolute';
+  flyImg.id = 'fly';
+  //THIS FAZ REFERENCIA AO PROPRIO ELEMENTO HTML IMG
+  flyImg.onclick = function(){
+    this.remove();
+  };
+
+  //ÁRVORE DE ELEMENTOS / CHILD
+  document.body.appendChild(flyImg);
+
+ 
+  randomFlySize();
+
+}
+
+//TAMANHO RANDÔMICO PARA O MOSQUITO
+//MATH.RANDOM GERA NÚMEROS ALEATÓRIO ENTRE 0 E 1
+function randomFlySize(){
+  let classSize = Math.floor(Math.random() * 3);
+
+  if(classSize === 0){
+    return 'fly-sm'
+  }else if(classSize === 1){
+    return 'fly-md'
+  }else{
+    return 'fly-lg'
+  };
+
+  // ALTERNATIVA AO IF
+  // switch(classSize){
+  //   case 0:
+  //   return 'fly-sm'
+  //   case 1:
+  //   return 'fly-md'
+  //   case 2:
+  //   return 'fly-lg'
+  // }
+};
+
+function randomSide(){
+  let classSide = Math.floor(Math.random() * 2);
+
+  if(classSide === 0){
+    return 'side-A'
+  }else{
+    return 'side-B'
+  };
+};
+
+
+
+
+
+
+
